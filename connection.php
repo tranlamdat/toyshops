0089a3595cbfdd5508a211b6c329a8e3f6acbc88
tranlamdat <dattlgbc200045@fpt.edu.vn>
2023-06-21 15:58:32 +0700

    fix bug

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
-$host = "ec2-52-2-167-43.compute-1.amazonaws.com";      // Change this if your PostgreSQL server is running on a different host
+$host = "ec2-34-236-103-63.compute-1.amazonaws.com";      // Change this if your PostgreSQL server is running on a different host
 $port = "5432";           // Change this if your PostgreSQL server is using a different port
-$dbname = "deugjphov0ten7";
-$user = "qklvaqhwxzsgov";
-$password = "29c3a6d9fe80d0342e275374ac0ca1b0fdbbc2649c710b01f76d0f89c5a4742b";
+$dbname = "dcljkn9969g8si";
+$user = "iomiqwbnnigjwo";
+$password = "7a873b91d11c2853ff776dbc933992eea722b35beba4fb224a964763dc0481b9";
 
 // Connect to the PostgreSQL database
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
